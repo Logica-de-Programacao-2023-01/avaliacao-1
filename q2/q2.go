20573c8cbe2b596dbd9f82d525903d7002638f65
gadelima <gabriel.delima@mercadolibre.com>
2023-05-01 22:07:35 -0300

    feat: solution

--- q2/q2.go
+++ q2/q2.go
@@ -13,6 +13,17 @@ package q2
 //Ajude os amigos a encontrar o número de problemas para os quais eles escreverão uma solução.
 
 func ProblemsSolved(answers [][3]bool) int {
-	// Seu código aqui
-	return 0
+	var solved int
+	for _, answer := range answers {
+		var count int
+		for _, hasSolution := range answer {
+			if hasSolution {
+				count++
+			}
+		}
+		if count >= 2 {
+			solved++
+		}
+	}
+	return solved
 }
